26f20e11cb0db20340b8dc5f335926636893ce6b
denisdalmeida04@gmail.com <denisdalmeida04@gmail.com>
2020-11-17 15:38:12 +0100

    denisjunior
    project initialisation

---
+++ veterinaire_G10/src/main/java/com/example/veterinaire_G10/VeterinaireG10Application.java
@@ -0,0 +1,13 @@
+package com.example.veterinaire_G10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaireG10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaireG10Application.class, args);
+	}
+
+}
