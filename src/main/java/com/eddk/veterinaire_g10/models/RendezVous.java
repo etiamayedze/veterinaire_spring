531d1d857c7135c2909d6f4a99ccdf9da64fb73a
Straw-Etiam <etiam.ayedze@gmail.com>
2020-12-12 17:03:55 +0100

    etiamv1

--- src/main/java/com/eddk/veterinaire_g10/models/RendezVous.java
+++ src/main/java/com/eddk/veterinaire_g10/models/RendezVous.java
@@ -33,6 +33,70 @@ public class RendezVous {
     private String heure_debut_rdv;
     private String heure_fin_rdv;
 
+    public RendezVous(Animal animal, Medecin medecin, List<Ordonnance> ordonnances, String date_rdv, String heure_debut_rdv, String heure_fin_rdv) {
+        this.animal = animal;
+        this.medecin = medecin;
+        this.ordonnances = ordonnances;
+        this.date_rdv = date_rdv;
+        this.heure_debut_rdv = heure_debut_rdv;
+        this.heure_fin_rdv = heure_fin_rdv;
+    }
+
+    public int getRdv_id() {
+        return rdv_id;
+    }
+
+    public void setRdv_id(int rdv_id) {
+        this.rdv_id = rdv_id;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public Medecin getMedecin() {
+        return medecin;
+    }
+
+    public void setMedecin(Medecin medecin) {
+        this.medecin = medecin;
+    }
+
+    public List<Ordonnance> getOrdonnances() {
+        return ordonnances;
+    }
+
+    public void setOrdonnances(List<Ordonnance> ordonnances) {
+        this.ordonnances = ordonnances;
+    }
+
+    public String getDate_rdv() {
+        return date_rdv;
+    }
+
+    public void setDate_rdv(String date_rdv) {
+        this.date_rdv = date_rdv;
+    }
+
+    public String getHeure_debut_rdv() {
+        return heure_debut_rdv;
+    }
+
+    public void setHeure_debut_rdv(String heure_debut_rdv) {
+        this.heure_debut_rdv = heure_debut_rdv;
+    }
+
+    public String getHeure_fin_rdv() {
+        return heure_fin_rdv;
+    }
+
+    public void setHeure_fin_rdv(String heure_fin_rdv) {
+        this.heure_fin_rdv = heure_fin_rdv;
+    }
 
     public RendezVous() {
     }
