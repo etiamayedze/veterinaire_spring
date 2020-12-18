d3189220545ae03fe91655f4e3e09528b8fab9da
Straw-Etiam <etiam.ayedze@gmail.com>
2020-12-18 22:54:24 +0100

    security permitAll

--- src/main/java/com/eddk/veterinaire_g10/controllers/SecurityConfig.java
+++ src/main/java/com/eddk/veterinaire_g10/controllers/SecurityConfig.java
@@ -29,28 +29,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
         OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
         OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-        jwtDecoder.setJwtValidator(withAudience);
-        return jwtDecoder;
-    }
-
-
-
-        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
 
         jwtDecoder.setJwtValidator(withAudience);
 
         return jwtDecoder;
     }
-
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .mvcMatchers("/api/v0/medecins").permitAll()
-
                 .mvcMatchers("/api/v0/animals").permitAll()
                 .mvcMatchers("/api/v0/rendezvous").permitAll()
                 .mvcMatchers("/api/v0/medicaments").permitAll()
@@ -58,9 +47,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/api/v0/parents").permitAll()
                 .mvcMatchers("/api/v0/prescription").permitAll()
                 .mvcMatchers("/api/v0/typeanimal").permitAll()
-
                 .and().cors()
                 .and().oauth2ResourceServer().jwt()
         ;
     }
 }
+
+
